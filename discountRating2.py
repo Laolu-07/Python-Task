900f608653b6e579796c1592241a7ca2ea723b7e
Laolu-07 <laoluofgod9@gmail.com>
2026-04-17 20:14:02 +0100

    Add files via upload

---
+++ discountRating2.py
@@ -0,0 +1,8 @@
+price = int (input(' what is your total spendings :')) 
+
+if price >= 1000 and price <= 10000:
+   print ('your discount is = 5% : ')
+elif price >= 10000 and price <= 50000:
+   print ('your discount is = 10% :')
+elif price >= 50000:
+   print ('your discount is = 20% :') 
