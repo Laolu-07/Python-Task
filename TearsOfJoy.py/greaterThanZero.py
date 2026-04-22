bd146b5f89b2ec5b5a72048f63be2cd33a75ae8d
Laolu-07 <laoluofgod9@gmail.com>
2026-04-22 08:40:14 +0100

    Add files via upload

---
+++ TearsOfJoy.py/greaterThanZero.py
@@ -0,0 +1,4 @@
+number = 0
+while (number <= 0):
+   number = int(input("enter a number"))
+print ("you entered", number) 
---
+++ TearsOfJoy.py/sevenTimetable.py
@@ -0,0 +1,2 @@
+for times in range (13):
+   print ("7 X", times, "=", 7 * times) 
